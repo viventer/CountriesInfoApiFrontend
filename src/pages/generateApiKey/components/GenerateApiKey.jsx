0ee073bd2019c76d77f0963d53e3fa3ddb8bcd3a
viventer <viventermail@gmail.com>
2023-08-25 14:02:28 +0200

    started adding api key generator

--- src/pages/generateApiKey/components/GenerateApiKey.jsx
+++ src/pages/generateApiKey/components/GenerateApiKey.jsx
@@ -1,3 +1,20 @@
+import { useGetApiKeysQuery } from "../slices/apiKeysSlice";
+import { StyledGenerateApiKey } from "../styles/GenerateApiKey.styled";
+
+import HashLoader from "react-spinners/HashLoader";
+
 export default function GenerateApiKey() {
-  return <div>GenerateApiKey</div>;
+  const {
+    data: apiKeys,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetApiKeysQuery();
+
+  let content;
+
+  if (isLoading) content = <HashLoader color="fff" />;
+
+  return <StyledGenerateApiKey></StyledGenerateApiKey>;
 }
---
+++ src/pages/generateApiKey/slices/apiKeysSlice.js
@@ -0,0 +1,72 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../../app/api/apiSlice";
+
+const keysAdapter = createEntityAdapter({});
+
+const initialState = keysAdapter.getInitialState();
+
+export const apiKeysSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getApiKeys: builder.query({
+      query: () => ({
+        url: "/api-key/all",
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
+      transformResponse: (responseData) => {
+        return keysAdapter.setAll(initialState, responseData);
+      },
+      providesTags: (result, error, arg) => [
+        { type: "ApiKey", id: "LIST" },
+        ...result.ids.map((id) => ({ type: "ApiKey", id })),
+      ],
+    }),
+    generateApiKey: builder.query({
+      query: () => ({
+        url: "/api-key/generate",
+        method: "GET",
+      }),
+      invalidatesTags: [{ type: "ApiKey", id: "LIST" }],
+    }),
+    deleteApiKeys: builder.mutation({
+      query: () => ({
+        url: "/api-key/all",
+        method: "DELETE",
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "ApiKey", id: "LIST" }],
+    }),
+    deleteApiKey: builder.mutation({
+      query: ({ id }) => ({
+        url: `/api-key/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "ApiKey", id: arg.id }],
+    }),
+  }),
+});
+
+export const {
+  useGetApiKeysQuery,
+  useGenerateApiKeyQuery,
+  useDeleteApiKeysMutation,
+  useDeleteApiKeyMutation,
+} = apiKeysSlice;
+
+// returns the query result object
+export const selectApiKeysResult = apiKeysSlice.endpoints.getApiKeys.select();
+
+// creates memoized selector
+const selectApiKeysData = createSelector(
+  selectApiKeysResult,
+  (apiKeysResult) => apiKeysResult.data // normalized state object with ids & entities
+);
+
+//getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+  selectAll: selectAllApiKeys,
+  selectIds: selectApiKeyIds,
+  // Pass in a selector that returns the notes slice of state
+} = keysAdapter.getSelectors(
+  (state) => selectApiKeysData(state) ?? initialState
+);
